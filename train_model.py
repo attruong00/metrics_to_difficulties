ecede1fd0eba2148d95ad80716862eac6540a138
Abbey Truong <57568294+attruong00@users.noreply.github>
2020-08-23 14:51:14 -0500

    delete extra dataset class

--- train_model.py
+++ train_model.py
@@ -9,21 +9,6 @@ from torch.utils.data import Dataset, DataLoader
 from data_process import MetricsDataset
 
 
-
-class MyDataSet(Dataset):
-
-    # inputs are the diff metrics
-    # outputs are the timed results
-    def __init__(self, inputs, outputs):
-        self.inputs = torch.as_tensor(inputs).float()
-        self.outputs = torch.as_tensor(outputs).float()
-
-    def __len__(self):
-        return len(self.inputs)
-
-    def __getitem__(self, index):
-        return self.inputs[index], self.outputs[index]
-
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
@@ -41,14 +26,14 @@ class Net(nn.Module):
 
 def main():
     # create dataset and split between train and test sets
-    my_data = MetricsDataset('../diff_files/', '../nn_input_dwa_only.npy', '../path_files/')
+    my_data = MetricsDataset('../diff_files/', '../nn_input_1.npy', '../path_files/')
     train_data, test_data = torch.utils.data.random_split(my_data, [250, 50])
     print("Train dataset size:", len(train_data))
     print("Test dataset size:", len(test_data))
 
     # load data
     train_loader = DataLoader(train_data, batch_size=250, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=50, shuffle=False)
 
     net = Net()
     lossf = nn.MSELoss()
@@ -77,9 +62,9 @@ def main():
         # currently not using idx but leaving it there in case
         for idx, (x, y) in enumerate(test_loader):
             pred = net(x)
-            # loss = lossf(pred, y)
-            # print(loss)
-            print(pred, y)
+            loss = lossf(pred, y)
+            print(loss)
+            # print(pred, y)
 
 
 if __name__ == "__main__":
