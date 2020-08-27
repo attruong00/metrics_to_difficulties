f817e08d2304e82d7e4a16d8531acff5ea6ca46f
Abbey Truong <57568294+attruong00@users.noreply.github>
2020-08-27 17:46:57 -0500

    final parameters, test on phys data

--- train_model.py
+++ train_model.py
@@ -13,34 +13,42 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.fc1 = nn.Linear(5, 64)
-        self.fc2 = nn.Linear(64, 16)
-        self.fc3 = nn.Linear(16, 1)
+        self.fc1 = nn.Linear(5, 1)
+        # self.fc2 = nn.Linear(64, 1)
+        # self.fc3 = nn.Linear(16, 1)
         # self.fc4 = nn.Linear(16, 1)
         # self.fc5 = nn.Linear(16, 4)
         # self.fc6 = nn.Linear(4, 2)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
         # x = F.relu(self.fc3(x))
         # x = F.relu(self.fc4(x))
         # x = F.relu(self.fc5(x))
-        x = self.fc3(x)
+        x = self.fc1(x)
         return x
 
 
-def main(penalty=35, alpha=0.001, epochs=10_000):
+def main(penalty=30, alpha=0.01, epochs=1_000):
     # create dataset and split between train and test sets
     metrics_dir = '../norm_metrics_files/'
     results_file = 'time_results_10/penalty_%d_means.npy' % penalty
+    test_metrics_dir = '../test_data/norm_metrics_files/'
+    test_results_file = '../test_data/phys_results.npy'
+
+    """
     my_data = MetricsDataset(metrics_dir, results_file)
     train_data, test_data = torch.utils.data.random_split(my_data, [250, 50])
     
+    """
+    train_data = MetricsDataset(metrics_dir, results_file)
+    test_data = MetricsDataset(test_metrics_dir, test_results_file)
+    
 
     # load data
-    train_loader = DataLoader(train_data, batch_size=250, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=1, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size=300, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
 
     net = Net()
     lossf = nn.MSELoss()
@@ -50,7 +58,7 @@ def main(penalty=35, alpha=0.001, epochs=10_000):
     print('*****************************')
     print('Train dataset size:', len(train_data))
     print('Test dataset size:', len(test_data))
-    print('Layers', 3)
+    print('Layers', 1)
     print('Penalty', penalty)
     print('Learning rate', alpha)
     print('Epochs', epochs)
@@ -72,24 +80,28 @@ def main(penalty=35, alpha=0.001, epochs=10_000):
             optimizer.step()
             
             if i == epochs - 1:
-                print('final training loss:', loss.item())
+                print(f'final training loss {i}:', loss.item())
     
     print('Testing...')
     with torch.no_grad():
 
         # test model
-        # currently not using idx but leaving it there in case
         running_loss = 0.0
+        predictions = []
         for idx, (x, y) in enumerate(test_loader):
             pred = net(x)
             pred = torch.squeeze(pred, 1)
             loss = lossf(pred, y)
+
+            # keep track of predictions and loss
             running_loss += loss
+            predictions.append(pred.item())
             # print(loss)
-            # print(x, y, pred)
+            print(idx, x, y.item(), pred.item())
+
         test_loss = running_loss.item() / 50.0
         print('test loss:', test_loss)
-        return test_loss
+        return predictions
 
 
 if __name__ == "__main__":
